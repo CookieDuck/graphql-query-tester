402b219ff35a690a5514afaddf60d7ee1e19444c
Paul Ganster <paul.ganster@dish.com>
2019-07-29 16:20:04 -0600

    Initial tests and impl for tokenizer.parse

---
+++ test/tokenizer.js
@@ -0,0 +1,34 @@
+const expect = require('chai').expect;
+const tokenizer = require('../lib/tokenizer');
+
+describe('String tokenizer', function() {
+  describe('Removes whitespace', function() {
+    it('Handles huge swaths of whitespace between tokens', function() {
+      const result = tokenizer.parse(`{           files                     }`);
+      expect(result).to.eql(['{', 'files', '}'])
+    });
+
+    it('Removes leading and trailing whitespace', function() {
+      const result = tokenizer.parse(`             { files }           `);
+      expect(result).to.eql(['{', 'files', '}'])
+    });
+
+    it('Handles newline characters', function() {
+      const result = tokenizer.parse(`\n\n   \n
+        {  \n
+   \n     files \n
+        }
+      \n\n\n`);
+      expect(result).to.eql(['{', 'files', '}'])
+    });
+
+    it('Handles carriage return characters', function() {
+      const result = tokenizer.parse(`\r\r   \r
+        {  \r
+   \r     files \r
+        }
+      \r\r\r`);
+      expect(result).to.eql(['{', 'files', '}'])
+    });
+  });
+});
