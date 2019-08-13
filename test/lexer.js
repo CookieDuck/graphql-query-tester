6ec83ff6ef213407da083ff5a75644a990fee3a3
Paul Ganster <paul.ganster@dish.com>
2019-08-13 11:14:56 -0600

    Lex multiple arguments and handle int, float, enum, and str arguments.

--- test/lexer.js
+++ test/lexer.js
@@ -48,6 +48,122 @@ describe('Lexer for tokens', function() {
     expect(depths).to.eql([0, 1, 1, 2, 1, 0]);
   });
 
+  describe('Arguments', function() {
+    describe('Definitions per type', function() {
+      it('String', function() {
+        const graphql = '{ files(extension: "txt") }';
+        const result = lexer.lex(tokenizer.tokenize(graphql));
+        const definitions = result.map((item) => item.definition);
+        expect(definitions.slice(2, -2)).to.eql([
+          dict.ARGUMENT_START,
+          dict.ARGUMENT_NAME,
+          dict.COLON,
+          dict.QUOTES,
+          dict.ARGUMENT_VALUE,
+          dict.QUOTES,
+        ]);
+      });
+
+      it('Int', function() {
+        const graphql = '{ files(limit: 3) }';
+        const result = lexer.lex(tokenizer.tokenize(graphql));
+        const definitions = result.map((item) => item.definition);
+        expect(definitions.slice(2, -2)).to.eql([
+          dict.ARGUMENT_START,
+          dict.ARGUMENT_NAME,
+          dict.COLON,
+          dict.ARGUMENT_VALUE,
+        ]);
+      });
+
+      it('Float', function() {
+        const graphql = '{ files(coolnessThreshold: 2.4) }';
+        const result = lexer.lex(tokenizer.tokenize(graphql));
+        const definitions = result.map((item) => item.definition);
+        expect(definitions.slice(2, -2)).to.eql([
+          dict.ARGUMENT_START,
+          dict.ARGUMENT_NAME,
+          dict.COLON,
+          dict.ARGUMENT_VALUE,
+        ]);
+      });
+
+      it('Enum', function() {
+        const graphql = '{ files(encoding: UTF_8) }';
+        const result = lexer.lex(tokenizer.tokenize(graphql));
+        const definitions = result.map((item) => item.definition);
+        expect(definitions.slice(2, -2)).to.eql([
+          dict.ARGUMENT_START,
+          dict.ARGUMENT_NAME,
+          dict.COLON,
+          dict.ARGUMENT_VALUE,
+        ]);
+      });
+    });
+
+    it('Assigns definitions to argument tokens', function() {
+      const graphql = '{ files(extension: "txt") }';
+      const result = lexer.lex(tokenizer.tokenize(graphql));
+      const definitions = result.map((item) => item.definition);
+      expect(definitions).to.eql([
+        dict.GROUP_START,
+        dict.FIELD_SCALAR,
+        dict.ARGUMENT_START,
+        dict.ARGUMENT_NAME,
+        dict.COLON,
+        dict.QUOTES,
+        dict.ARGUMENT_VALUE,
+        dict.QUOTES,
+        dict.ARGUMENT_END,
+        dict.GROUP_END,
+      ]);
+    });
+
+    it('Assigns values to argument tokens', function() {
+      const graphql = '{ files(extension: "txt", limit: 3) }';
+      const result = lexer.lex(tokenizer.tokenize(graphql));
+      const values = result.map((item) => item.value);
+      expect(values).to.eql([
+        '{',
+        'files',
+        '(',
+        'extension',
+        ':',
+        '"',
+        'txt',
+        '"',
+        ',',
+        'limit',
+        ':',
+        '3',
+        ')',
+        '}',
+      ]);
+    });
+
+    it('Handles multiple arguments on a field', function() {
+      const graphql = '{ files(extension: "txt", limit: 3) }';
+      const result = lexer.lex(tokenizer.tokenize(graphql));
+      const definitions = result.map((item) => item.definition);
+      expect(definitions).to.eql([
+        dict.GROUP_START,
+        dict.FIELD_SCALAR,
+        dict.ARGUMENT_START,
+        dict.ARGUMENT_NAME,
+        dict.COLON,
+        dict.QUOTES,
+        dict.ARGUMENT_VALUE,
+        dict.QUOTES,
+        dict.COMMA,
+        dict.ARGUMENT_NAME,
+        dict.COLON,
+        dict.ARGUMENT_VALUE,
+        dict.ARGUMENT_END,
+        dict.GROUP_END,
+      ]);
+    });
+  });
+
   describe('Lexing errors', function() {
     describe('Uneven curly braces', function() {
       it('Too many open curly braces', function() {
